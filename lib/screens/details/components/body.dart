e88bc9db1991505ae44fa4da2c34549177817be0
Abu Anwar <abuanwar072@gmail.com>
2020-09-20 19:54:31 +0600

    details page done

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,30 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/constants.dart';
+import 'package:shop_app/components/default_button.dart';
 import 'package:shop_app/models/Product.dart';
 import 'package:shop_app/size_config.dart';
 
+import 'color_dots.dart';
+import 'product_description.dart';
+import 'top_rounded_container.dart';
 import 'product_images.dart';
 
 class Body extends StatelessWidget {
   final Product product;
 
   const Body({Key key, @required this.product}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         ProductImages(product: product),
-        Container(
-          margin: EdgeInsets.only(top: 20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40),
-              topRight: Radius.circular(40),
-            ),
-          ),
+        TopRoundedContainer(
+          color: Colors.white,
           child: Column(
-            children: [],
+            children: [
+              ProductDescription(
+                product: product,
+                pressOnSeeMore: () {},
+              ),
+              TopRoundedContainer(
+                color: Color(0xFFF6F7F9),
+                child: Column(
+                  children: [
+                    ColorDots(product: product),
+                    TopRoundedContainer(
+                      color: Colors.white,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: SizeConfig.screenWidth * 0.15,
+                          right: SizeConfig.screenWidth * 0.15,
+                          bottom: getProportionateScreenWidth(40),
+                          top: getProportionateScreenWidth(15),
+                        ),
+                        child: DefaultButton(
+                          text: "Add To Cart",
+                          press: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ],
---
+++ lib/screens/details/components/color_dots.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/rounded_icon_btn.dart';
+import 'package:shop_app/models/Product.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class ColorDots extends StatelessWidget {
+  const ColorDots({
+    Key key,
+    @required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    // Now this is fixed and only for demo
+    int selectedColor = 3;
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: Row(
+        children: [
+          ...List.generate(
+            product.colors.length,
+            (index) => ColorDot(
+              color: product.colors[index],
+              isSelected: index == selectedColor,
+            ),
+          ),
+          Spacer(),
+          RoundedIconBtn(
+            icon: Icons.remove,
+            press: () {},
+          ),
+          SizedBox(width: getProportionateScreenWidth(20)),
+          RoundedIconBtn(
+            icon: Icons.add,
+            showShadow: true,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  const ColorDot({
+    Key key,
+    @required this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final Color color;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 2),
+      padding: EdgeInsets.all(getProportionateScreenWidth(8)),
+      height: getProportionateScreenWidth(40),
+      width: getProportionateScreenWidth(40),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        border:
+            Border.all(color: isSelected ? kPrimaryColor : Colors.transparent),
+        shape: BoxShape.circle,
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
