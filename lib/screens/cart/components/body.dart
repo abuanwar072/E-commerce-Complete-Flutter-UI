afbdefed9ed20c76cbb6a4fbe3bf0ba25d3e246c
Abu Anwar <abuanwar072@gmail.com>
2020-09-24 10:23:45 +0600

    Check Out Page done

---
+++ lib/screens/cart/components/body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:shop_app/models/Cart.dart';
+
+import '../../../size_config.dart';
+import 'cart_card.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+        itemCount: demoCarts.length,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Dismissible(
+            key: Key(demoCarts[index].product.id.toString()),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              setState(() {
+                demoCarts.removeAt(index);
+              });
+            },
+            background: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xFFFFE6E6),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Row(
+                children: [
+                  Spacer(),
+                  SvgPicture.asset("assets/icons/Trash.svg"),
+                ],
+              ),
+            ),
+            child: CartCard(cart: demoCarts[index]),
+          ),
+        ),
+      ),
+    );
+  }
+}
