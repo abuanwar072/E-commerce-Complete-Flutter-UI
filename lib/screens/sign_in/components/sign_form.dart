8640f961ddede6a63dbf908125ded2081a868f9a
itech_faisal <faisal.ahmed@setuptech.international>
2020-12-23 19:19:02 +0600

    keyboard open padding crash problem fixed A RenderFlex overflowed by 73 pixels on the bottom. E-commerce-Complete-Flutter-UI/lib/screens/login_success/components/body.dart:9:12 E-commerce-Complete-Flutter-UI/lib/screens/details/components/body.dart:9:18

--- lib/screens/sign_in/components/sign_form.dart
+++ lib/screens/sign_in/components/sign_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/components/custom_surfix_icon.dart';
 import 'package:shop_app/components/form_error.dart';
+import 'package:shop_app/helper/keyboard.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
 
@@ -75,6 +76,7 @@ class _SignFormState extends State<SignForm> {
               if (_formKey.currentState.validate()) {
                 _formKey.currentState.save();
                 // if all are valid then go to success screen
+                KeyboardUtil.hideKeyboard(context);
                 Navigator.pushNamed(context, LoginSuccessScreen.routeName);
               }
             },
