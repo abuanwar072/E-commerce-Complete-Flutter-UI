3924d2146d33b6d10938bcc541e78cdb38efea4e
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-11-16 20:01:00 +0600

    Otp & Profile screen updated

--- lib/screens/otp/otp_screen.dart
+++ lib/screens/otp/otp_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import '../../size_config.dart';
 
-import 'components/body.dart';
+import '../../constants.dart';
+import '../../size_config.dart';
+import 'components/otp_form.dart';
 
 class OtpScreen extends StatelessWidget {
   static String routeName = "/otp";
@@ -14,7 +15,49 @@ class OtpScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("OTP Verification"),
       ),
-      body: const Body(),
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.05),
+                Text(
+                  "OTP Verification",
+                  style: headingStyle,
+                ),
+                const Text("We sent your code to +1 898 860 ***"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("This code will expired in "),
+                    TweenAnimationBuilder(
+                      tween: Tween(begin: 30.0, end: 0.0),
+                      duration: const Duration(seconds: 30),
+                      builder: (_, dynamic value, child) => Text(
+                        "00:${value.toInt()}",
+                        style: const TextStyle(color: kPrimaryColor),
+                      ),
+                    ),
+                  ],
+                ),
+                const OtpForm(),
+                SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    // OTP code resend
+                  },
+                  child: const Text(
+                    "Resend OTP Code",
+                    style: TextStyle(decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'components/body.dart';
+import 'components/profile_menu.dart';
+import 'components/profile_pic.dart';
 
 class ProfileScreen extends StatelessWidget {
   static String routeName = "/profile";
@@ -12,7 +13,40 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Profile"),
       ),
-      body: const Body(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          children: [
+            const ProfilePic(),
+            const SizedBox(height: 20),
+            ProfileMenu(
+              text: "My Account",
+              icon: "assets/icons/User Icon.svg",
+              press: () => {},
+            ),
+            ProfileMenu(
+              text: "Notifications",
+              icon: "assets/icons/Bell.svg",
+              press: () {},
+            ),
+            ProfileMenu(
+              text: "Settings",
+              icon: "assets/icons/Settings.svg",
+              press: () {},
+            ),
+            ProfileMenu(
+              text: "Help Center",
+              icon: "assets/icons/Question mark.svg",
+              press: () {},
+            ),
+            ProfileMenu(
+              text: "Log Out",
+              icon: "assets/icons/Log out.svg",
+              press: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
