be7f8f40d6b9edd6ac1e062efa0cf776a057f906
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-11-16 20:20:26 +0600

    bug Fixed

--- lib/components/form_error.dart
+++ lib/components/form_error.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../size_config.dart';
-
 class FormError extends StatelessWidget {
   const FormError({
     Key? key,
@@ -24,8 +22,8 @@ class FormError extends StatelessWidget {
       children: [
         SvgPicture.asset(
           "assets/icons/Error.svg",
-          height: getProportionateScreenWidth(14),
-          width: getProportionateScreenWidth(14),
+          height: 16,
+          width: 16,
         ),
         SizedBox(
           width: 10,
