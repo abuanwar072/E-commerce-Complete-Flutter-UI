ab04651858e7cb312df28f091282be238c84af6d
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-11-16 20:23:17 +0600

    Done V2

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/init_screen.dart';
 
-import '../home/home_screen.dart';
-
 class LoginSuccessScreen extends StatelessWidget {
   static String routeName = "/login_success";
 
