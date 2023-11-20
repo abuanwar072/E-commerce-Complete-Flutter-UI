8efabea472031abcb3317bfaa52cd222bbb440f1
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-11-20 17:44:56 +0600

    ElavatedButton theme updated

--- lib/theme.dart
+++ lib/theme.dart
@@ -29,6 +29,7 @@ class AppTheme {
         style: ElevatedButton.styleFrom(
           elevation: 0,
           backgroundColor: kPrimaryColor,
+          foregroundColor: Colors.white,
           minimumSize: const Size(double.infinity, 48),
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(16)),
